668b77d0e5cfe6584f170c5986c3d44099beac2f
Michael Louie <michael.louie10@gmail.com>
2018-01-21 19:15:45 -0500

    Initial commit

---
+++ src/main/java/com/MyTicketService.java
@@ -0,0 +1,34 @@
+package main.java.com;
+
+public class MyTicketService implements  TicketService {
+    /**
+     * The number of seats in the venue that are neither held nor reserved
+     *
+     * @return the number of tickets available in the venue
+     */
+    public int numSeatsAvailable() {
+        return -1;
+    }
+    /**
+     * Find and hold the best available seats for a customer
+     *
+     * @param numSeats the number of seats to find and hold
+     * @param customerEmail unique identifier for the customer
+     * @return a SeatHold object identifying the specific seats and related
+    information
+     */
+    public SeatHold findAndHoldSeats(int numSeats, String customerEmail) {
+        return null;
+    }
+    /**
+     * Commit seats held for a specific customer
+     *
+     * @param seatHoldId the seat hold identifier
+     * @param customerEmail the email address of the customer to which the
+    seat hold is assigned
+     * @return a reservation confirmation code
+     */
+    public String reserveSeats(int seatHoldId, String customerEmail) {
+        return null;
+    }
+}
